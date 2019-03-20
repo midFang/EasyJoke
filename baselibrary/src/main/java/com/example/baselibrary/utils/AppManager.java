ec961a958af833770be0acc1e3f172367203d4a6
fangsf <highestfeng@gmail.com>
2019-03-20 22:28:36 +0800

    appmanager 修改

--- baselibrary/src/main/java/com/example/baselibrary/utils/AppManager.java
+++ baselibrary/src/main/java/com/example/baselibrary/utils/AppManager.java
@@ -97,5 +97,32 @@ public class AppManager {
         }
     }
 
+
+    private static long exitTime = 0;
+
+    /**
+     * 作用如：2s内双击两次返回则退出程序
+     *
+     * @return 是否退出程序
+     */
+    public static boolean exitBy2Click() {
+        return exitBy2Click(2000);
+    }
+
+    /**
+     * 在某个时间段内双击两次
+     *
+     * @param space 两次点击最大时间间隔
+     * @return 是否退出
+     */
+    public static boolean exitBy2Click(int space) {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            exitTime = System.currentTimeMillis();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
 
